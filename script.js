4ac6b528f8382c5da09c83f29652e5322ee84fcb
gaslight77 <dnp773692003@gmail.com>
2025-04-18 14:32:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function showit(var1) {
+  // hide the one div is shown
+  document.getElementById("tab1").style.display = "none"
+  document.getElementById("tab2").style.display = "none"
+  document.getElementById("tab3").style.display = "none"
+  document.getElementById("tab4").style.display = "none"
+  document.getElementById("tab5").style.display = "none"
+  document.getElementById("tab6").style.display = "none"
+  // show the one div that you sent to this function
+  document.getElementById(var1).style.display = "block"
+}
 
