97b4618b2c2b37d50bad902d6cfd2d13a59f230c
gaslight77 <dnp773692003@gmail.com>
2025-04-25 23:18:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ function showit(var1) {
   document.getElementById(var1).style.display = "block"
 }
 
+function myfunction() {
+const var2 = document.getElementById('textbox1').value
+const var3 = document.getElementById('textbox2').value
+const var4 = document.getElementById('textbox3').value
+const var5 = document.getElementById('textbox4').value
+document.getElementById('stuff').innerText = var2 + " " + var3 + " " + var4 + " " + var5
+}
